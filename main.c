12ca9e4cfadb1848c4266173a5f19bfc39f19402
prudhviyadala <122338807+prudhviyadala@users.noreply.github.com>
2023-04-12 12:40:23 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -5,23 +5,29 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-
-#include <stdio.h>
-
-int main() {
-    int num;
-    printf("Input number: ");
-    scanf("%d", &num);
-    
-    if (num > 0) {
-        printf("%d is positive", num);
-    }
-    else if (num < 0) {
-        printf("%d is negative", num);
-    }
-    else {
-        printf("%d is zero", num);
-    }
-    
-    return 0;
+#include<stdio.h>
+int main(){
+int n,i;
+int fact,rem;
+printf("Enter a number : ");
+scanf("%d",&n);
+printf(".");
+int sum = 0;
+int temp = n;
+while(n){
+ i = 1,fact = 1;
+ rem = n % 10;
+ while(i <= rem){
+fact = fact * i;
+i++;
+ }
+ sum = sum + fact;
+ n = n / 10;
 }
+if(sum == temp)
+printf("%d is a strong number.",temp);
+else
+ printf("%d is not a strong number.",temp);
+return 0;
+}
+
